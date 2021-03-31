44d95f07cfbeb7cae886a413b316c59d2ed8c781
sharvyemalwar <69106145+sharvyemalwar@users.noreply.github.com>
2021-03-31 12:39:19 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,134 @@
+/*var Engine = Matter.Engine;
+  World = Matter.World;
+  Events = Matter.Events;
+  Bodies = Matter.Bodies;*/
+  const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+var engine, world;
+  var frameCount
+ 
+var particles = [];
+var plinkos = [];
+var divisions = [];
+var particle = [];
+var score=0;
+var turn=0;
+var play;
+var gamestate = play;
+
+var divisionHeight=300;
+var line =(0,500);
+
+
+function setup() {
+  createCanvas(800, 800);
+
+  engine = Engine.create();
+  world = engine.world;
+  ground = new Ground(width/2,height,width,20);
+
+    text("score:20,20");
+
+
+   for (var k = 0; k <=width; k = k + 80) {
+     divisions.push(new Divisions(k, height-divisionHeight/2, 10, divisionHeight));
+   }
+
+
+    for (var j = 75; j <=width; j=j+50) 
+    {
+    
+       plinkos.push(new Plinko(j,75));
+    }
+
+    for (var j = 50; j <=width-10; j=j+50) 
+    {
+    
+       plinkos.push(new Plinko(j,175));
+    }
+
+     for (var j = 75; j <=width; j=j+50) 
+    {
+    
+       plinkos.push(new Plinko(j,275));
+    }
+
+     for (var j = 50; j <=width-10; j=j+50) 
+    {
+    
+       plinkos.push(new Plinko(j,375));
+    }
+
+    
+
+    
+}
+ 
+
+
+function draw() {
+  background("black");
+  textSize(20)
+ text("Score : "+score,20,20);
+ text("500",20,520);
+ text("500",100,520);
+ text("500",180,520);
+ text("500",260,520);
+ text("100",340,520);
+ text("100",420,520);
+ text("100",500,520);
+ text("200",580,520);
+ text("200",660,520);
+ text("200",740,520);
+
+ //line.display();
+
+
+  Engine.update(engine);
+
+  //particle.mousePressed();
+ 
+  
+   for (var i = 0; i < plinkos.length; i++) {
+     
+     plinkos[i].display();
+     
+   }
+  
+  for (var j = 0; j < particles.length; j++) {
+   
+     particles[j].display();
+   }
+   for (var k = 0; k < divisions.length; k++) {
+     
+     divisions[k].display();
+   }
+}
+if(frameCount%60===0){
+  particles.push(new particle(random(width/2-30, width/2+30), 10,10));
+  score++;
+}
+
+function mousePressed()
+ {
+
+if(gamestate!=="end"){
+
+count++;
+
+particle=new Particle(mouseX, 10, 10, 10);
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
